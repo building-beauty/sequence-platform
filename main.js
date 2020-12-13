7aea939afb06d7847ed7090484feaf59d1c59c40
Greg Bryant <greg@rainmagazine.com>
2020-12-13 00:43:32 -0800

    close to first version for step 1

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function central_handler () {
 function init () {
     // start from the JSON structure 'start'
     // put into element 'body'
-    process (start,"body");
+    process(start);
 }
 
 // ***************************************************************
@@ -46,39 +46,57 @@ function init () {
 // process the NEXT item in the ESSENCE
 //
 
-function process (obj,target_id) {
-    var target_element = document.getElementById(target_id);
+function process(obj) {
 
     // ITERATE THROUGH THE FEATURES HERE
 
-    if (obj["type"] == "replace_html") {
-       f_replace(obj,target_id);
+    if (obj["feature"] == "set_of_cards") {
+       f_element(obj);
+       f_set_of_cards(obj);
     }
 
-    if (obj["type"] == "set_of_cards") {
-       f_element(obj,target_id);
-       f_set_of_cards(obj,target_id);
+    if (obj["feature"] == "card") {
+       f_element(obj);
+       f_card(obj);
     }
 
-    if (obj["type"] == "card") {
-       f_element(obj,target_id);
-       f_card(obj,target_id);
+    if (obj["feature"] == "button") {
+       f_element(obj);
+       f_button(obj);
     }
 
-    if (obj["type"] == "button") {
-       f_element(obj,target_id);
-       f_button(obj,target_id);
+    if (obj["feature"] == "text") {
+       f_element(obj);
+       f_text(obj);
     }
 
-    if (obj["type"] == "text") {
-       f_element(obj,target_id);
-       f_text(obj,target_id);
+    if (obj["feature"] == "image") {
+	var n_div = document.createElement('img');
+	document.getElementById(obj["target_id"]).appendChild(n_div);
+	n_div.className = obj["feature"];
+	n_div.innerHTML = obj["content"];
+	n_div.id = obj["id"];
+	n_div.src = obj["src"];
+    }
+
+    if (obj["feature"] == "redefine") {
+	n_div = document.getElementById(obj["id"]);
+ 	n_div.innerHTML = obj["content"];
+	if ("switch_click" in obj) {
+	    n_div.onclick = n_div.ontouch = function(event) {
+		var cards = document.getElementsByClassName("card");
+		for (var i = 0; i < cards.length; i ++) {
+		    cards[i].style.display = 'none';
+		}     
+		document.getElementById(obj["switch_click"]).style.display = "block";
+	    }    
+	}
     }
 
     // CALL PROCESS ON ANY JSON ARRAY
     if (Array.isArray(obj)) {
        for (var x of obj) {
-           process (x,target_id);
+           process (x);
        }
     }
 }
@@ -87,29 +105,46 @@ function process (obj,target_id) {
 // FEATURES
 //
 
-function f_replace(obj, target_id) {
-       document.getElementById(obj["id"]).innerHTML = obj["content"];
-}
-
-function f_element(obj, target_id) {
+function f_element(obj) {
  var n_div = document.createElement('div');
- document.getElementById(obj["id"]).appendChild(n_div);
- n_div.className = obj["type"];
+ document.getElementById(obj["target_id"]).appendChild(n_div);
+ n_div.className = obj["feature"];
+ n_div.innerHTML = obj["content"];
+ n_div.id = obj["id"];
+ if ("start" in obj) {
+     if (obj["start"] == "hidden") {
+	 n_div.style.display = "none";
+     }
+ }
+ if ("switch_click" in obj) {
+     n_div.onclick = n_div.ontouch = function(event) {
+	 var cards = document.getElementsByClassName("card");
+	 for (var i = 0; i < cards.length; i ++) {
+	     cards[i].style.display = 'none';
+	 }     
+	 document.getElementById(obj["switch_click"]).style.display = "block";
+     }
+ }
+ if ("click_process" in obj) {
+     n_div.onclick = n_div.ontouch = function(event) {
+	 process(window[obj["click_process"]]);
+     }
+ }
 }
 
-function f_set_of_cards(obj, target_id) {
+function f_set_of_cards(obj) {
 //
 }
 
-function f_card(obj, target_id) {
+function f_card(obj) {
 //
 }
 
-function f_button(obj, target_id) {
+function f_button(obj) {
 //
 }
 
-function f_text(obj, target_id) {
+function f_text(obj) {
 //
 }
 
@@ -122,22 +157,58 @@ function f_text(obj, target_id) {
 // (a kind of option-oriented programming)
 //
 
-var start = main_page();
+var start = [
+         {
+         "feature":"card",
+	 "id":"card1",
+         "target_id":"root",
+         "content":"card1"
+	 },
 
-function main_page () {
+         {
+         "feature":"button",
+	 "id":"button1",
+         "target_id":"card1",
+         "content":"to card2",
+	 "switch_click":"card2"
+	 },
+         {
+         "feature":"card",
+	 "id":"card2",
+         "target_id":"root",
+         "content":"card2",
+	 "start":"hidden"
+	 },
 
-    var main_page = {
-         "type":"replace_html",
-         "id":"root",
-         "content":"Sequence Center"
-    };
+         {
+         "feature":"button",
+	 "id":"button2",
+         "target_id":"card2",
+         "content":"load photo",
+	 "click_process":"load_the_photo"
+	 }
+		      ]
+    ;
 
-    return main_page;
-}
+    var load_the_photo = [
+		{
+		    "feature":"image",
+		    "target_id":"card2",
+		    "id":"image1",
+		    "src":"chanterelle.jpg"
+		},
+		{
+		    "feature":"redefine",
+		    "id":"button2",
+		    "content":"to card1",
+		    "switch_click":"card1"
+		}
+	       ];
+
 
 // ***************************************************************
 // Storage of user actions
 //
 // A pre-stub. When the user does something, we will store it here.
-// and eventyually put it on the server.
+// and eventually put it on the server.
 //
